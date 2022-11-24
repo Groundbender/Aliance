c4f65cf4225cbc6ca10910bdaa7526a2e7a1ac7f
Groundbender <damir24may@mail.ru>
2022-11-24 03:34:50 +0300

    fix: images

--- index.php
+++ index.php
@@ -165,23 +165,35 @@
   <div class="swiper-wrapper">
     <!-- Slides -->
     <a href="#"  class="swiper-slide blog-card">
-    <img src="img/blog-photo.jpg" 
-    alt="" 
+    <picture   >
+      <source   type="image/webp" srcset="img/blog-photo.webp">
+      <source   type="image/jpeg" srcset="img/blog-photo.jpg">
+      <img src="img/blog-photo.jpg" 
+    alt="Карточка блога " 
     class="blog-card-image">
+        </picture>
       <h3 class="blog-card-title" >Современная методология разработки одухотворила всех причастных</h3>
       <p class="blog-card-text">Действия представителей оппозиции, превозмогая сложившуюся непростую экономическую ситуацию, в равной степени предоставлены...</p>
     </a>
     <a href="#" class="swiper-slide blog-card">
-        <img src="img/blog-photo2.jpg" 
-        alt="" 
-        class="blog-card-image">
+    <picture   >
+      <source   type="image/webp" srcset="img/blog-photo2.webp">
+      <source   type="image/jpeg" srcset="img/blog-photo2.jpg">
+      <img src="img/blog-photo2.jpg" 
+    alt="Карточка блога 2" 
+    class="blog-card-image">
+        </picture>
       <h3 class="blog-card-title" >Сложно сказать, почему жизнь прекрасна</h3>
       <p class="blog-card-text">Сложно сказать, почему элементы политического процесса функционально разнесены на независимые элементы. Безусловно, высокотехнологичная...</p>
     </a>
     <a href="#" class="swiper-slide blog-card">
+    <picture   >
+      <source   type="image/webp" srcset="img/blog-photo.webp">
+      <source   type="image/jpeg" srcset="img/blog-photo.jpg">
       <img src="img/blog-photo.jpg" 
-      alt="" 
-      class="blog-card-image">
+    alt="Карточка блога " 
+    class="blog-card-image">
+        </picture>
         <h3 class="blog-card-title" >Современная методология разработки одухотворила всех причастных</h3>
         <p class="blog-card-text">Действия представителей оппозиции, превозмогая сложившуюся непростую экономическую ситуацию, в равной степени предоставлены...</p>
     </a>
